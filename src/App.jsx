07069b8b46f3c302f0b22620b167675325b653ed
ahmad2207 <136051325+ahmad2207@users.noreply.github.com>
2025-09-01 08:48:27 +0100

    fix typo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
-import stem from "./pages/stem";
+import Stem from "./pages/Stem";
 import AboutPage from "./pages/AboutPage";
 import FAQPage from "./pages/FAQPage";
 
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/stem" element={<stem />} />
+        <Route path="/stem" element={<Stem />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/faq" element={<FAQPage />} />
       </Routes>
